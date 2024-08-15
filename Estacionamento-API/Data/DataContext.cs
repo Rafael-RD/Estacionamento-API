9818afad3410fbffbda4f52cca40ebd469b9cf5f
Rafael-PC <rafaelreinertd@hotmail.com>
2024-08-15 17:45:59 -0300

    Add project files.

---
+++ Estacionamento-API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Estacionamento_API.Data
+{
+    public class DataContext : DbContext
+    {
+        public object DbPath { get; }
+
+        public DataContext()
+        {
+            DbPath = "Estacionamento.db";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite($"Data Source={DbPath}");
+
+
+    }
+}
