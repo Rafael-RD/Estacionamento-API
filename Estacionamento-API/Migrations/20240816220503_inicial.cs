af291929928154fe7b305c0c9deab2f6c9d88f6a
Rafael-PC <rafaelreinertd@hotmail.com>
2024-08-16 19:06:47 -0300

    feat: cração migração

---
+++ Estacionamento-API/Migrations/20240816220503_inicial.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Estacionamento_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Precos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PrecoFixo = table.Column<int>(type: "INTEGER", nullable: false),
+                    PrecoHora = table.Column<int>(type: "INTEGER", nullable: false),
+                    PeriodoInicio = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    PeriodoFinal = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Precos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Veiculos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Placa = table.Column<string>(type: "TEXT", nullable: false),
+                    DataEntrada = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataSaida = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Precos");
+
+            migrationBuilder.DropTable(
+                name: "Veiculos");
+        }
+    }
+}
