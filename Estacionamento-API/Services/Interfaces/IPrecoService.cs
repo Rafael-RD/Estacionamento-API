bd09cdc2e406378908d24b5bf9122f54a932cb95
Rafael-PC <rafaelreinertd@hotmail.com>
2024-08-16 19:22:21 -0300

    feat: Criação PrecoService e interface

---
+++ Estacionamento-API/Services/Interfaces/IPrecoService.cs
@@ -0,0 +1,17 @@
+﻿using Estacionamento_API.Models;
+
+namespace Estacionamento_API.Services.Interfaces
+{
+    public interface IPrecoService
+    {
+        Task<PrecoModel?> GetPrecoAtual();
+        Task<IEnumerable<PrecoModel>> GetPrecoTodos();
+        Task<PrecoModel?> GetPrecoPorId(int id);
+
+        Task PostPreco(PrecoModel preco);
+
+        Task PutPreco(int id, PrecoModel preco);
+
+        Task DeletePreco(int id);
+    }
+}
---
+++ Estacionamento-API/Services/PrecoService.cs
@@ -0,0 +1,38 @@
+﻿using Estacionamento_API.Models;
+using Estacionamento_API.Services.Interfaces;
+
+namespace Estacionamento_API.Services
+{
+    public class PrecoService : IPrecoService
+    {
+        public Task<PrecoModel?> GetPrecoAtual()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PrecoModel?> GetPrecoPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PrecoModel>> GetPrecoTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PostPreco(PrecoModel preco)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PutPreco(int id, PrecoModel preco)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletePreco(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
