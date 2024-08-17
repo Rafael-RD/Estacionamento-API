ac9344c8615e96d7676344939e78c2fc9abe284d
Rafael-PC <rafaelreinertd@hotmail.com>
2024-08-16 23:20:45 -0300

    feat: PrecoService adicionado ao escopo

--- Estacionamento-API/Program.cs
+++ Estacionamento-API/Program.cs
@@ -1,3 +1,6 @@
+using Estacionamento_API.Services;
+using Estacionamento_API.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IPrecoService, PrecoService>();
 
 var app = builder.Build();
 
