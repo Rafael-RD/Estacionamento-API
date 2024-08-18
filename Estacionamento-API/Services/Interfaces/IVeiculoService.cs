38f52dc0503f7f4d8cd7c6c1dac9ed268e8de36d
Rafael-PC <rafaelreinertd@hotmail.com>
2024-08-18 15:13:10 -0300

    feat(interface): IVeiculoService

---
+++ Estacionamento-API/Services/Interfaces/IVeiculoService.cs
@@ -0,0 +1,20 @@
+﻿using Estacionamento_API.Models;
+using Estacionamento_API.Models.DTOs;
+
+namespace Estacionamento_API.Services.Interfaces
+{
+    public interface IVeiculoService
+    {
+        Task<VeiculoModel?> GetVeiculoPorId(int id);
+        Task<VeiculoModel?> GetVeiculoPorPlaca(string placa);
+        Task<IEnumerable<VeiculoModel>> GetVeiculoTodos();
+        Task<IEnumerable<VeiculoModel>> GetVeiculoEstacionados();
+
+        Task PostVeiculoEntrada(VeiculoEntradaDTO veiculoEntradaDTO);
+        Task PostVeiculoSaida(VeiculoSaidaDTO veiculoSaidaDTO);
+
+        Task PutVeiculo(int id, VeiculoModel veiculo);
+
+        Task DeleteVeiculo(int id);
+    }
+}
