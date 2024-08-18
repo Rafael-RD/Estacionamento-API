0eb283ecd58d5f8e7b8d7501cd7b2d220bb17942
Rafael-PC <rafaelreinertd@hotmail.com>
2024-08-18 19:18:56 -0300

    feat(DTO): VeiculoSaidaPrecoDTO

---
+++ Estacionamento-API/Models/DTOs/VeiculoSaidaPrecoDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Estacionamento_API.Models.DTOs
+{
+    public class VeiculoSaidaPrecoDTO
+    {
+        public required string Placa { get; set; }
+        public DateTime DataEntrada { get; set; }
+
+        public int PrecoFixo { get; set; }
+        public int PrecoHora { get; set; }
+        public int APagar { get; set; }
+    }
+}
