5248ad6ea45593132f7ea89ad0e40be41cb54da6
Rafael-PC <rafaelreinertd@hotmail.com>
2024-08-19 02:46:08 -0300

    feat(controller): try/catch adicionado

--- Estacionamento-API/Controllers/VeiculoController.cs
+++ Estacionamento-API/Controllers/VeiculoController.cs
@@ -21,60 +21,116 @@ namespace Estacionamento_API.Controllers
         [HttpGet("id/{id}")]
         public async Task<ActionResult<VeiculoModel>> GetVeiculoPorId(int id)
         {
-            var veiculo = await _veiculoService.GetVeiculoPorId(id);
+            try
+            {
+                var veiculo = await _veiculoService.GetVeiculoPorId(id);
 
-            if (veiculo == null) return NotFound("Veiculo não encontrado");
+                if (veiculo == null) return NotFound("Veiculo não encontrado");
 
-            return Ok(veiculo);
+                return Ok(veiculo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("placa/{placa}")]
         public async Task<ActionResult<VeiculoModel?>> GetVeiculoPorPlaca(string placa)
         {
-            var veiculo = await _veiculoService.GetVeiculoPorPlaca(placa);
+            try
+            {
+                var veiculo = await _veiculoService.GetVeiculoPorPlaca(placa);
 
-            if (veiculo == null) return NotFound("Veiculo não encontrado");
+                if (veiculo == null) return NotFound("Veiculo não encontrado");
 
-            return Ok(veiculo);
+                return Ok(veiculo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("estacionados")]
         public async Task<ActionResult<IEnumerable<VeiculoModel>>> GetVeiculoEstacionados()
         {
-            return Ok(await _veiculoService.GetVeiculoEstacionados());
+            try
+            {
+                return Ok(await _veiculoService.GetVeiculoEstacionados());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("todos")]
         public async Task<ActionResult<IEnumerable<VeiculoModel>>> GetVeiculoTodos()
         {
-            return Ok(await _veiculoService.GetVeiculoTodos());
+            try
+            {
+                return Ok(await _veiculoService.GetVeiculoTodos());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPost("entrada")]
         public async Task<ActionResult<string>> PostVeiculoEntrada(VeiculoEntradaDTO veiculoEntradaDTO)
         {
-            await _veiculoService.PostVeiculoEntrada(veiculoEntradaDTO);
-            return Ok("Veiculo adicionado");
+            try
+            {
+                await _veiculoService.PostVeiculoEntrada(veiculoEntradaDTO);
+                return Ok("Veiculo adicionado");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPost("saida")]
         public async Task<ActionResult<VeiculoSaidaPrecoDTO>> PostVeiculoSaida(VeiculoSaidaDTO veiculoSaidaDTO)
         {
-            return Ok(await _veiculoService.PostVeiculoSaida(veiculoSaidaDTO));
+            try
+            {
+                return Ok(await _veiculoService.PostVeiculoSaida(veiculoSaidaDTO));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult<string>> PutVeiculo(int id, VeiculoModel veiculo)
         {
-            await _veiculoService.PutVeiculo(id, veiculo);
-            return Ok("Veiculo atualizado");
+            try
+            {
+                await _veiculoService.PutVeiculo(id, veiculo);
+                return Ok("Veiculo atualizado");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<string>> DeleteVeiculo(int id)
         {
-            await _veiculoService.DeleteVeiculo(id);
-            return Ok("Veiculo deletado");
+            try
+            {
+                await _veiculoService.DeleteVeiculo(id);
+                return Ok("Veiculo deletado");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
